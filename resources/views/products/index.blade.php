ef3ae8a43ab34336b8a32959029c7a939b418971
AdrianaSantorio <adriana.santorio@gmail.com>
2022-03-24 16:21:57 +0100

    working on card in index.blade

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,17 +1,32 @@
 @extends('layouts.main')
 
 @section('content')
-<ul>
-    @foreach ($products as $product)
-    <li>
-        <figure>
-            <img src="{{ $product->image }}" alt="">
-        </figure>
-        <h2>{{ $product->name }}</h2>
-        <p>{{ $product->description }}</p>
-        <div>{{ $product->price }}</div>
-        <a href="{{ route('products.show', ['product' => $product->id]) }}">Dettagli</a>
-    </li>
-    @endforeach
-</ul>
+<main>
+    <div class="container">
+        <div id="product-gallery" class="row g-4">
+            @foreach ($products as $product)
+            <div class="col-6">
+                <div class="product-card d-flex flex-row justify-between">
+                    <div class="card-image col-5">
+                        <figure>
+                            <img src="{{ $product->image }}" alt="{{ $product->name }}" class="img-fluid">
+                        </figure>
+                    </div>
+                    <div class="card-text col-7 p-1 d-flex flex-column justify-between">    
+                        <div>
+                            <h5>{{ $product->name }}</h5>
+                            <p class="text-justify">{{ $product->description }}</p>
+                            <p class="text-end">{{ $product->price }}</p>
+                        </div>
+                        <div class="text-end">
+                            <a href="{{ route('products.show', ['product' => $product->id]) }}" class="btn btn-small btn-primary">Dettagli</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</main>
+
 @endsection
